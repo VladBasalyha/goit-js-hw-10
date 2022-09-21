51b6e40a0a736e9d7d1d80bf4e0eddda5bff5c16
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2022-09-21 17:35:13 +0300

    отримав розмітку з фетч

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import './css/styles.css';
 import API from './js/fetchCountries';
 import { debounce } from 'throttle-debounce';
+const DEBOUNCE_DELAY = 300;
 
 const refs = {
 	inputText: document.querySelector('#search-box'),
 	countryList: document.querySelector('.country-list'),
 	countryInfo: document.querySelector('.country-info'),
 };
-
-refs.inputText.addEventListener(
-	'input',
-	debounce(1000, e => API.fetchСountries(e.target.value))
-);
 const { inputText, countryList, countryInfo } = refs;
-const DEBOUNCE_DELAY = 300;
+
+inputText.addEventListener('input', debounce(DEBOUNCE_DELAY, onSearchInput));
+
+function onSearchInput(evt) {
+	return API.fetchСountries(evt.target.value).then(countries =>
+		createCountriesMarkup(countries)
+	);
+}
+
+function createCountriesMarkup(countries) {
+	const markup = countries
+		.map(country => {
+			return `
+			<li class = country-item>
+			<img src = ${country.flags.svg} width = 40 height = 30> </img>
+			<span class = "country-name">${country.name.official}</span>
+			</li>`;
+		})
+		.join('');
+	countryList.innerHTML = markup;
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 function fetchСountries(name) {
-	return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response =>
-		console.log(response.json())
-	);
+	return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+		return response.json();
+	});
 }
 export default { fetchСountries };
